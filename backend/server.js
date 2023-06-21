9186a83ef6de660229072be1a9aa59677163d505
andikastp <andikapangestu79@gmailcom>
2023-06-21 19:06:57 +0700

    revert to non ES module, adding routes to GET, POST, DELETE, Update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-import express from 'express';
-import 'dotenv/config'
+const express = require('express');
+require('dotenv').config();
+const router = express.Router()
+const workoutRoutes = require('./routes/workout')
 
 const app = express(); // creating express app
+
 
 //middleware
 app.use((req, res, next) => {
@@ -10,9 +13,9 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.get('/', (req, res) => {
- res.json({mssg:'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
+
+
 
 app.listen(process.env.PORT, () => { // listen for request
   console.log(`listening on port:${process.env.PORT}`);
