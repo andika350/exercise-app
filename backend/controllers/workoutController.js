876838041cbb40814ff27b3152d373fb07aee01f
andikastp <andikapangestu79@gmailcom>
2023-06-22 10:56:47 +0700

    added delete and update controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -38,11 +38,45 @@ const createWorkout = async (req, res) => {
 };
 
 //delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  const workout = await Workout.findByIdAndDelete({ _id: id })
+  
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(workout)
+}
 
 //update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  const workout = await Workout.findOneAndUpdate({ _id: id }, {
+    ...req.body
+  })
+
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(workout);
+}
 
 module.exports = {
   createWorkout,
   getWorkouts,
   getWorkout,
+  deleteWorkout,
+  updateWorkout
 };
