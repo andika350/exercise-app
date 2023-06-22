57af6a5a145652514403a4a23b3b6eb934603231
andikastp <andikapangestu79@gmailcom>
2023-06-22 09:38:17 +0700

    added model schema with mongoose, created data to db

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,21 +1,29 @@
-const express = require('express');
+const express = require("express");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
 //GET all workouts
-router.get('/', (req,res) => {
-res.json({message: 'GET all workouts'})
-})
+router.get("/", (req, res) => {
+  res.json({ message: "GET all workouts" });
+});
 
 //GET a single workout
-router.get('/:id', (req, res) => {
-  res.json({message:'GET a single workout'})
-})
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET a single workout" });
+});
 
 //POST a new workout
-router.post('/', (req, res) => {
-  res.json({message:'POST a new workout'})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps })
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({message:error.message})
+  }
+});
 
 //DELETE a workout
 router.delete("/:id", (req, res) => {
